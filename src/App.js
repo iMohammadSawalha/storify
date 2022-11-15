14bd942cc6d46ec994f1faf9f3e53e611b6b3dd6
Osama Abureesh <92232795+OsamaAbuReesh@users.noreply.github.com>
2022-11-15 03:12:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Client from './components/Client';
+import Dates from './components/Dates';
+import Footer from './components/Footer';
+// import Navigation from './components/Navigation';
+import Table from './components/Table';
+import './components/style.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation />
+      <Client />
+      <Table />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
