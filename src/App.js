21ad1ab6d12cffdfabe0a30e8d6e82a9cbce9bb6
Mohammed-AbuZahra <s12113186@stu.najah.edu>
2022-11-16 11:30:51 +0200

    second week progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from "react";
-import Navbar from "./Components/Navbar";
-
+import Navbar from "./Components/Navbar/Navbar";
 function App() {
   return (
-    // Using the newly created Header
-    // component in this main component
     <Navbar />
     );
 }
