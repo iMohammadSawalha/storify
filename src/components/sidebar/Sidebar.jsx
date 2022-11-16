fed3cbb23d4b4362e37dd12071fb5eeff2babac4
Ayman Sawalha <[200~s12144191@stu.najah.edugit config --global user.email s12144191@stu.najah.edu>
2022-11-16 11:30:48 +0200

    initial commit

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,84 @@
+import "./sidebar.scss"
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import AddHomeOutlinedIcon from '@mui/icons-material/AddHomeOutlined';
+import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
+import PeopleOutlineOutlinedIcon from '@mui/icons-material/PeopleOutlineOutlined';
+import ReceiptLongOutlinedIcon from '@mui/icons-material/ReceiptLongOutlined';
+import BadgeOutlinedIcon from '@mui/icons-material/BadgeOutlined';
+import SettingsSuggestOutlinedIcon from '@mui/icons-material/SettingsSuggestOutlined';
+import AnalyticsOutlinedIcon from '@mui/icons-material/AnalyticsOutlined';
+import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
+import StoreMallDirectoryOutlinedIcon from '@mui/icons-material/StoreMallDirectoryOutlined';
+
+
+
+export const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+        <div className="top">
+            <StoreMallDirectoryOutlinedIcon className="icon-main"/>
+            <span className="logo">Gentlemen's Store</span>
+        </div>
+        <hr />
+        <div className="centre">
+        <ul>
+                <h1 className="title-main">Main Menu</h1>
+                <p className="title">MAIN</p>
+
+                <li>
+                    <AddHomeOutlinedIcon className="icon"/>
+                    <span>Home</span>
+                </li>
+                <li>
+                <DashboardIcon className="icon"/>
+                    <span>Dashbaord</span>
+                </li>
+                <p className="title">SCOPE</p>
+
+                <li>
+                    <AddShoppingCartOutlinedIcon className="icon"/>
+                    <span>Products</span>
+                </li>
+
+
+                <li>
+                        <PeopleOutlineOutlinedIcon className="icon"/>
+                    <span>Customers</span>
+                </li>
+
+                <li>
+                    <CalendarMonthOutlinedIcon className="icon"/>
+                    <span>Calendar</span>
+                </li>
+
+
+            <p className="title">DATA</p>
+
+                <li>
+                    <ReceiptLongOutlinedIcon className="icon"/>
+                    <span>Invoices</span>
+                </li>
+
+                <li>
+                    <AnalyticsOutlinedIcon className="icon"/>
+                    <span>Analysis</span>
+                </li>
+
+            <p className="title">SETTINGS</p>
+
+                <li>
+                    <BadgeOutlinedIcon className="icon"/>
+                    <span>Profile</span>
+                </li>
+                <li>
+                    <SettingsSuggestOutlinedIcon className="icon"/>
+                    <span>Settings</span>
+                </li>
+        </ul>
+           
+        </div>
+    </div>
+
+
+  );
+}
