f4a2a8958cf503202adf7ac987329ad4b663a229
OsamaM <92232795+OsamaAbuReesh@users.noreply.github.com>
2022-11-23 16:13:06 +0300

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = ({ total, tax }) => {
             <div className="row p-4">
                 <div className="container-fluid">
                     <p className="fw-bold col-4">Sub Total: <span>{total}$</span> </p>
-                    <p className="fw-bold col-4">Tax: <span> {tax}$</span> </p>
+                    <p className="fw-bold col-4">Tax: <span>{tax}$</span> </p>
                     <div className="buttons ">
                         <button className="btn col-1 btn-outline-primary" onClick={btnPrint}>Print</button>
                     </div>
