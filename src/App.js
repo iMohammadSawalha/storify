d05c54619ca5618e23b26160da33b1a0486e24d4
mohammad-e-sawalha <s12112755@stu.najah.edu>
2022-11-26 14:13:42 +0200

    Adding OrderTables using mui

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import SideBar from "./Components/sidebar/Sidebar";
-import Analysis from "./pages/Analysis/Analysis";
+import Analysis from "./pages/Analysis/AnalysisPage";
 
 function App() {
   return (
     <div>
       <Navbar/>
       <div className="d-flex">
-        <SideBar/>
+          <SideBar/>
         <div className="pageinfo">
           <Analysis/>
         </div>
