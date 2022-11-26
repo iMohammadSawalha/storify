3b06791b55032f1d43495d69ef298715c58e71a3
Mohammed-AbuZahra <s12113186@stu.najah.edu>
2022-11-27 01:04:31 +0200

    third week progress

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import CollapsibleExample from "./Components/Navbar/Navbar";
 import NavBar from "./Components/Navbar/Navbar";
 function App() {
   return (
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,30 +1,31 @@
 //Import
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { ChatBubbleOutline, LanguageOutlined, NotificationsActiveOutlined, NotificationsNoneOutlined, SearchOffOutlined, SearchOutlined } from '@mui/icons-material';
-import { Avatar } from '@mui/material';
-
+import { LanguageOutlined, SearchOutlined } from '@mui/icons-material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
+import VpnKeyIcon from '@mui/icons-material/VpnKey';
+import LogoutIcon from '@mui/icons-material/Logout';
 import "./Navbar.css"
 const NavBar = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="white" variant="white" >
+    <Navbar expand="lg" bg="light">
       <Container>
-        <Navbar.Brand>
-          <Form className="d-flex">
-            <Form.Control
-            border="0.5px"
-              type="search"
-              placeholder="Search..."
-              className="me-1"
-              aria-label="Search"
-            />
-            <Button variant="outline-dark"><SearchOutlined /></Button>
-          </Form></Navbar.Brand>
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search..."
+            className="me-1"
+          />
+          <Button variant="outline-dark"><SearchOutlined /></Button>
+        </Form>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
 
         <Navbar.Collapse id="responsive-navbar-nav">
@@ -32,47 +33,40 @@ const NavBar = () => {
           <Nav className="me-auto">
 
           </Nav>
-
-
           <Nav>
 
-            <Nav.Link ><LanguageOutlined />English</Nav.Link>
-
-
-            <NavDropdown title=<ChatBubbleOutline /> id="notification" align="end"  >
-              <NavDropdown.Item >Ahmed sent a message (3)min ago</NavDropdown.Item>
-              <NavDropdown.Item >Saeed sent a message (58)min ago</NavDropdown.Item>
-              <NavDropdown.Item >Husam sent a message (2)hour ago</NavDropdown.Item>
+            <NavDropdown title=<LanguageOutlined /> >
+              <NavDropdown.Item>No Other Available Language</NavDropdown.Item>
+            </NavDropdown>
+            English
+            <NavDropdown title=<ChatIcon /> id="messages" align="end"  >
+              <NavDropdown.Item > <AccountCircleIcon />Ahmed sent a message <AccessTimeFilledIcon />3 min ago</NavDropdown.Item>
+              <NavDropdown.Item ><AccountCircleIcon />Saleh sent a message <AccessTimeFilledIcon />58 min ago</NavDropdown.Item>
+              <NavDropdown.Item ><AccountCircleIcon />Husam sent a message <AccessTimeFilledIcon />2 hour ago</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item >
-                Inbox
+                <ChatIcon />Inbox
               </NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown alignleft title=<NotificationsNoneOutlined /> id="notifications" align="end">
-              <NavDropdown.Item  >Ahmed Added Product (3)min ago</NavDropdown.Item>
-
-              <NavDropdown.Item >Saeed Added Product (58)min ago</NavDropdown.Item>
-              <NavDropdown.Item >Husam Added Product (2)hour ago</NavDropdown.Item>
+            <NavDropdown title=<NotificationsIcon /> id="notifications" align="end">
+              <NavDropdown.Item  ><AccountCircleIcon />Mohammed Added Product <AccessTimeFilledIcon />3 min ago</NavDropdown.Item>
+              <NavDropdown.Item ><AccountCircleIcon />Hamza Added Product <AccessTimeFilledIcon />58 min ago</NavDropdown.Item>
+              <NavDropdown.Item ><AccountCircleIcon />Mazen Added Product <AccessTimeFilledIcon />2 hour ago</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item >
-                Inbox
+                <NotificationsIcon />All Notifications
               </NavDropdown.Item>
             </NavDropdown>
-
-
-            <NavDropdown title=<Avatar /> id="profile" align="end">
-
-              <NavDropdown.Item href="#action 1">
-                Profile
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action 2">Change Password </NavDropdown.Item>
+            <NavDropdown align="end" title=<AccountCircleIcon id="profile" /> >
+              <NavDropdown.Item >
+                <AccountCircleIcon /> Profile</NavDropdown.Item>
+              <NavDropdown.Item ><VpnKeyIcon /> Change Password </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action 3">
-                Logout
+              <NavDropdown.Item >
+                <LogoutIcon /> Logout
               </NavDropdown.Item>
             </NavDropdown>
-
-
+            Hi User &nbsp;&nbsp;
           </Nav>
         </Navbar.Collapse>
       </Container>
