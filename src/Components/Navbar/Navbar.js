4300cba8cf9a3b11683824fa5a71515e6af4d395
Mohammed-AbuZahra <s12113186@stu.najah.edu>
2022-11-29 21:25:16 +0200

    Added Routers

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
-//Import
+//Imports
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "./Navbar.css"
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -13,7 +14,8 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 import AccessTimeFilledIcon from '@mui/icons-material/AccessTimeFilled';
 import VpnKeyIcon from '@mui/icons-material/VpnKey';
 import LogoutIcon from '@mui/icons-material/Logout';
-import "./Navbar.css"
+import { NavLink } from 'react-bootstrap';
+//Function
 const NavBar = () => {
   return (
     <Navbar expand="lg" bg="light">
@@ -26,52 +28,42 @@ const NavBar = () => {
           />
           <Button variant="outline-dark"><SearchOutlined /></Button>
         </Form>
+
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-
         <Navbar.Collapse id="responsive-navbar-nav">
-
-          <Nav className="me-auto">
-
-          </Nav>
+          <Nav className="me-auto"></Nav>
           <Nav>
 
-            <NavDropdown title=<LanguageOutlined /> >
-              <NavDropdown.Item>No Other Available Language</NavDropdown.Item>
-            </NavDropdown>
+            <NavDropdown title=<LanguageOutlined /> ><NavDropdown.Item><LanguageOutlined /> No Other Available Language</NavDropdown.Item></NavDropdown>
             English
+
             <NavDropdown title=<ChatIcon /> id="messages" align="end"  >
-              <NavDropdown.Item > <AccountCircleIcon />Ahmed sent a message <AccessTimeFilledIcon />3 min ago</NavDropdown.Item>
+              <NavDropdown.Item >  <AccountCircleIcon />Ahmed sent a message <AccessTimeFilledIcon />3 min ago</NavDropdown.Item>
               <NavDropdown.Item ><AccountCircleIcon />Saleh sent a message <AccessTimeFilledIcon />58 min ago</NavDropdown.Item>
               <NavDropdown.Item ><AccountCircleIcon />Husam sent a message <AccessTimeFilledIcon />2 hour ago</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item >
-                <ChatIcon />Inbox
-              </NavDropdown.Item>
+              <NavDropdown.Item ><ChatIcon />Inbox</NavDropdown.Item>
             </NavDropdown>
+
             <NavDropdown title=<NotificationsIcon /> id="notifications" align="end">
-              <NavDropdown.Item  ><AccountCircleIcon />Mohammed Added Product <AccessTimeFilledIcon />3 min ago</NavDropdown.Item>
+            <NavDropdown.Item  ><AccountCircleIcon />Samer Added Product <AccessTimeFilledIcon />3 min ago</NavDropdown.Item>
               <NavDropdown.Item ><AccountCircleIcon />Hamza Added Product <AccessTimeFilledIcon />58 min ago</NavDropdown.Item>
               <NavDropdown.Item ><AccountCircleIcon />Mazen Added Product <AccessTimeFilledIcon />2 hour ago</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item >
-                <NotificationsIcon />All Notifications
-              </NavDropdown.Item>
+              <NavDropdown.Item ><NotificationsIcon />All Notifications</NavDropdown.Item>
             </NavDropdown>
+
             <NavDropdown align="end" title=<AccountCircleIcon id="profile" /> >
-              <NavDropdown.Item >
-                <AccountCircleIcon /> Profile</NavDropdown.Item>
+            <NavDropdown.Item ><AccountCircleIcon /> Profile</NavDropdown.Item>
               <NavDropdown.Item ><VpnKeyIcon /> Change Password </NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item >
-                <LogoutIcon /> Logout
-              </NavDropdown.Item>
-            </NavDropdown>
-            Hi User &nbsp;&nbsp;
+              <NavDropdown.Item ><LogoutIcon /> Logout</NavDropdown.Item>
+            </NavDropdown>Hi User &nbsp;&nbsp;
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
-
+//Exports
 export default NavBar;
