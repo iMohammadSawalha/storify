c8fa45c0a4d6966f3dc65c2b6f6299b7fe1eb2dc
Abdulrahman <s12114751@stu.najah.edu>
2022-12-04 00:26:22 +0200

    cards now are looking much better, they're also responsive

--- src/Components/Cards/CardItem.js
+++ src/Components/Cards/CardItem.js
@@ -1,17 +1,18 @@
 import Card from 'react-bootstrap/Card';
-import React, { useState } from "react";
+import React from "react";
 import './card.css';
-import DoughtnutChart from './DoughnutChart';
+import CardIcon from './CardIcon';
 
 export default function CardItem({info}) {
   return (
     <Card>
-      <Card.Body>
-        <div className='cardBody'>
-            <h4 id='cardTitle'>{info.title}</h4>
-            {info.chart && (<DoughtnutChart dimension={200}/>)}
-
+      <Card.Body className='d-flex ms-2'>
+        <div className='mainCardBody'>
+            <h5 id='cardTitle'>{info.title}</h5>
+            <Card.Title style={{fontWeight:'600'}}>{info.data}</Card.Title>
+            {/* {info.chart && (<DoughtnutChart dimension={200}/>)} */}
         </div>
+        <CardIcon options={{id:info.id}}/>
       </Card.Body>
     </Card>
   );
