604e24acb8377b07cf84aab7138c3178cb14d953
Ayman <s12144191@stu.najah.edu>
2022-12-10 12:43:15 +0200

    The new sidebar

--- src/Components/sidebar/Sidebar.js
+++ src/Components/sidebar/Sidebar.js
@@ -1,87 +1,100 @@
-import AddHomeOutlinedIcon from '@mui/icons-material/AddHomeOutlined';
-import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
-import AnalyticsOutlinedIcon from '@mui/icons-material/AnalyticsOutlined';
-import BadgeOutlinedIcon from '@mui/icons-material/BadgeOutlined';
-import CalendarMonthOutlinedIcon from '@mui/icons-material/CalendarMonthOutlined';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import PeopleOutlineOutlinedIcon from '@mui/icons-material/PeopleOutlineOutlined';
-import ReceiptLongOutlinedIcon from '@mui/icons-material/ReceiptLongOutlined';
-import SettingsSuggestOutlinedIcon from '@mui/icons-material/SettingsSuggestOutlined';
-import StoreMallDirectoryOutlinedIcon from '@mui/icons-material/StoreMallDirectoryOutlined';
+import React, { useState } from "react";
 import "./sidebar.css";
 
-
-
 const Sidebar = () => {
-  return (
-    <div className='sidebar'>
+    const [isExpanded, setExpendState] = useState(false);
+	const menuItems = [
+	
+        {
+			text: "Dashboard",
+			icon: "icons/grid.svg",
+		
+		},
+		{
+			text: "Products",
+			icon: "icons/shopping-cart.svg",
+		},
+		{
+			text: "Customers",
+			icon: "icons/user.svg",
+		},
+		{
+			text: "Analytics",
+			icon: "icons/pie-chart.svg",
+		},
+		{
+			text: "invoices",
+			icon: "icons/folder.svg",
+		},
+	
+		{
+			text: "Companies",
+			icon: "icons/download.svg",
+		},
+		{
+			text: "Settings",
+			icon: "icons/settings.svg",
+		},
+	];
+	return (
+		<div
+			className={
+				isExpanded
+					? "side-nav-container"
+					: "side-nav-container side-nav-container-NX"
+			}
+		>
+			<div className="nav-upper">
+				<div className="nav-heading">
+					{isExpanded && (
+						<div className="nav-brand">
+							<img src="icons/Logo.svg" alt="" srcset="" />
+							<h2>Storify</h2>
+						</div>
+					)}
+					<button
+						className={
+							isExpanded ? "hamburger hamburger-in" : "hamburger hamburger-out"
+						}
+						onClick={() => setExpendState(!isExpanded)}
+					>
+						<span></span>
+						<span></span>
+						<span></span>
+					</button>
+				</div>
+				<div className="nav-menu">
+					{menuItems.map(({ text, icon }) => (
+						<a
+							className={isExpanded ? "menu-item" : "menu-item menu-item-NX"}
+							href="#"
+						>
+							<img className="menu-item-icon" src={icon} alt="" srcset="" />
+							{isExpanded && <p>{text}</p>}
+						</a>
+					))}
+				</div>
+			</div>
+			<div className="nav-footer">
+				{isExpanded && (
+					<div className="nav-details">
+						<img
+							className="nav-footer-avatar"
+							src="icons/admin-avatar.svg"
+							alt=""
+							srcset=""
+						/>
+						<div className="nav-footer-info">
+							<p className="nav-footer-user-name">Ayman S.</p>
+							<p className="nav-footer-user-position">Store admin</p>
+						</div>
+					</div>
+				)}
+				<img className="logout-icon" src="icons/logout.svg" alt="" srcset="" />
+			</div>
+		</div>
+	);
+};
 
-        <div className="top">
-            <StoreMallDirectoryOutlinedIcon className="icon-main"/>
-            <span className="logo">Gentlemen's Store</span>
-        </div>
-        <hr />
-        <div className="centre">
-        <ul>
-                <h1 className="title-main">Main Menu</h1>
-                <p className="title">MAIN</p>
-
-                <li>
-                    <AddHomeOutlinedIcon className="icon"/>
-                    <span>Home</span>
-                </li>
-                <li>
-                <DashboardIcon className="icon"/>
-                    <span>Dashbaord</span>
-                </li>
-                <p className="title">SCOPE</p>
-
-                <li>
-                    <AddShoppingCartOutlinedIcon className="icon"/>
-                    <span>Products</span>
-                </li>
-
-
-                <li>
-                        <PeopleOutlineOutlinedIcon className="icon"/>
-                    <span>Customers</span>
-                </li>
-
-                <li>
-                    <CalendarMonthOutlinedIcon className="icon"/>
-                    <span>Calendar</span>
-                </li>
-
-
-            <p className="title">DATA</p>
-
-                <li>
-                    <ReceiptLongOutlinedIcon className="icon"/>
-                    <span>Invoices</span>
-                </li>
-
-                <li>
-                    <AnalyticsOutlinedIcon className="icon"/>
-                    <span>Analysis</span>
-                </li>
-
-            <p className="title">SETTINGS</p>
-
-                <li>
-                    <BadgeOutlinedIcon className="icon"/>
-                    <span>Profile</span>
-                </li>
-                <li>
-                    <SettingsSuggestOutlinedIcon className="icon"/>
-                    <span>Settings</span>
-                </li>
-        </ul>
-           
-        </div>
-    </div>
-
-
-  );
-}
 
 export default Sidebar;
