32dac965ecaa956e5f2d2744ad621c04c3cffb94
Abdulrahman <s12114751@stu.najah.edu>
2022-12-11 02:07:06 +0200

    minor changes

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -7,12 +7,16 @@ const Home = () => {
   const [cardData,setCardData] = useState(data);
   return (
     <>
-      <div className='row dashboardBox'>
-        {cardData.map((cardData)=>
-          <div className='col-lg-3 col-md-6 col-sm-9 col-xs-12 ps-lg-2 mb-3'>
-            <Card key={cardData.id} info={cardData} />
-          </div>
-        )}
+      <div className='row'>
+        <div className='col-6 mb-3'>
+          <Card key={cardData.id} info={cardData} />
+        </div>
+        <div className='col-lg-3 col-md-3 col-sm-3 col-xs-3 ps-lg-2 mb-3'>
+          <Card key={cardData.id} info={cardData} />
+        </div>
+        <div className='col-lg-3 col-md-3 col-sm-3 col-xs-3 ps-lg-2 mb-3'>
+          <Card key={cardData.id} info={cardData} />
+        </div>
       </div>
     </>
   )
