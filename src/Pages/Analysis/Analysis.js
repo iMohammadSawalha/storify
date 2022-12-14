72c80bc1c274b935cdb37fe69a90e30f7e731897
mohammad-e-sawalha <s12112755@stu.najah.edu>
2022-12-14 13:28:32 +0200

    The DataGrid Table now able to pull data from seperate Columns and Rows Files , Allwoing for Reusablity

--- src/Pages/Analysis/Analysis.js
+++ src/Pages/Analysis/Analysis.js
@@ -1,75 +1,10 @@
-import { Avatar } from '@mui/material';
-import { Button } from 'react-bootstrap';
 import AreaLineChartCard from '../../Components/Analysis/AreaLineChart';
 import DoughnutChartCard from '../../Components/Analysis/DoughnutChart';
 import GridTable from '../../Components/Analysis/GridTables/GridTable';
-import Orders from '../../Components/Analysis/GridTables/Orders-Latest.json';
 import RadarChartCard from '../../Components/Analysis/RadarChart';
 import './Analysis.css';
-const columns = [
-  { field: 'id', headerName: 'OrderID', minWidth: 90 ,type: 'number'},
-  {
-    field: 'user',
-    headerName: 'User Name',
-    minWidth: 150,
-    flex:2,
-    renderCell: (params) => {
-      return (
-        <>
-          <Avatar src={params.value.avatar}/>
-            {params.value.username}
-        </>
-      );
-    }
-  },
-  {
-    field: 'item-name',
-    headerName: 'Item Name',
-    minWidth: 110,
-    flex:2
-  },
-  {
-    field: 'quantity',
-    headerName: 'Quantity',
-    minWidth: 110,
-    flex:1,
-    type: 'number'
-  },
-  {
-    field: 'order-total',
-    headerName: 'Total',
-    minWidth: 110,
-    flex:1,
-    type: 'number',
-    renderCell: (params) => {
-      return (
-        <>
-            ${params.value}
-        </>
-      );
-    }
-  },
-  {
-    field: 'date',
-    headerName: 'Date',
-    minWidth: 160,
-    type:'date',
-  },
-  {
-    field: 'status',
-    headerName: 'Status',
-    minWidth: 150,
-    type:'number',
-    renderCell: (params) => {
-      if(params.value == 10)
-              return (<Button variant='btn btn-success btn-radius btn-sm'>Completed</Button>);
-              else if(params.value > 0)
-              return(<Button variant='btn btn-warning btn-radius btn-sm'>Pending</Button>);
-              else if(params.value == 0)
-              return (<Button variant='btn btn-outline-danger btn-radius btn-sm'>Cancelled</Button>)
-    }
-  },
-];
+import DataGridCol from "./DataGridColumn";
+import Orders from './Orders-Latest.json';
 const Analysis = () => {
   return (
     <div className='Analysis'>
@@ -78,7 +13,7 @@ const Analysis = () => {
         <AreaLineChartCard/>
         <RadarChartCard/>
         <div className='row'>
-         <GridTable tableTitle={"Latest Orders"}cols={columns} data={Orders}/> 
+         <GridTable tableTitle={"Latest Orders"} cols={DataGridCol} data={Orders}/> 
         </div>
       </div>
     </div>
