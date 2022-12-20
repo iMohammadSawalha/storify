e91023187a19a16d50cdfc7422b7d2c1075c8b26
Abdulrahman <s12114751@stu.najah.edu>
2022-12-20 11:22:55 +0200

    sidebar adjustments, added background pattern to website and tax calculator in progress..

--- src/components/TaxCalculator/Calc.js
+++ src/components/TaxCalculator/Calc.js
@@ -1,60 +1,30 @@
 import './Calc.css'
 import React, { useState } from 'react';
-import { Card } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form'
+import Card from 'react-bootstrap/Card';
 
-
-function Range() {
-    const [value1,SetValue1]=useState(0);
-    const [value2,SetValue2]=useState(0);
-
+function TaxCalc() {
+  const [Amount,SetAmount]=useState(0);
+  const [Percentage,SetPercentage]=useState(0);
     return(
-<>
-        <Card className='sliderCard'>
-        <Card.Header>
-          <h1 className='sliderLabel'>Tax Calculator</h1>
-        </Card.Header>
-
-        <Card.Body>
-    <label className='sliderLabel'> Income : </label>
-    <div className='flex2'></div>
-    <div className="slider-parent">
-    <span className='c'>$</span>
-    <input className='sliderValue' type="text" value={value1} />
-    <div className='flex2'></div>
-         <div>
-         <input  type="range" min="0" max="1000000" 
-         onChange={({ target: { value: radius } }) => { SetValue1(radius); }} />
-         </div>
-      </div>
-
-      <div className='flex'></div>
-
-      <label className='sliderLabel'> Tax percentage: </label>
-      <div className='flex2'></div>
-      <div className="slider-parent">
-      <span className='c'>%</span>
-    <input className='sliderValue' type="text" value={value2} />
-    <div className='flex2'></div>
-        <div>
-         <input type="range" min="0" max="100" 
-         onChange={({ target: { value: radius } }) => { SetValue2(radius);   }}/>
-         </div>
-      </div>
-    
-      <div className='flex'></div>
-      <h5 className='sliderLabel'> Total :</h5>
-      <span> {total} </span>
-
-
-
-
-
-      </Card.Body>
-
-
-  </Card>
-  </>
+      <>
+        <Card className=''>
+          <Card.Header>
+            <Form.Label className='my-auto'>Tax Calculator</Form.Label>
+          </Card.Header>
+          <Card.Body>
+            <span className='h6'>Amount {Amount}</span>
+            <Form.Range 
+            onChange={({ target: { value: radius } }) => { SetAmount(radius); }}
+            />
+            <span className='h6'>Tax Percentage {Percentage}</span>
+            <Form.Range 
+            onChange={({ target: { value: radius } }) => { SetPercentage(radius); }}
+            />
+          </Card.Body>
+        </Card>
+      </>
     )
 }
 
-export default Range;
+export default TaxCalc;
