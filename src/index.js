57ae23b44cc440a0d160462e91b1262392a141bf
Osama Moh <osama.aboresh7@gmail.com>
2022-12-20 18:06:40 +0200

    finishing responsive error-page and modifying some stuff

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
-import Sidebar from "./Components/Sidebar/Sidebar";
+import Sidebar from "./Components/sidebar/Sidebar";
 import './index.css';
 import Analysis from "./Pages/Analysis/Analysis";
 import Companies from "./Pages/Companies/Companies";
@@ -25,25 +25,25 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <BrowserRouter>
-    <Navbar/>
-        <div className="d-flex">
-        <Sidebar/>
-          <div className="pageinfo">
-            <Routes>        
-              <Route path='*' element={<ErrorPage />} />
-              <Route path="/home" index element={<Home/>} />
-              <Route path="/dashboard" element={<Dashboard/>} />
-              <Route path="/analysis" element={<Analysis/>} />
-              <Route path="/invoices" element={<Invoice/>} />
-              <Route path="/products" element={<Products/>} />
-              <Route path="/companies" element={<Companies/>} />
-              <Route path="/customers" element={<Customers/>} />
-              <Route path="/profile" element={<Profile/>} />
-              <Route path="/coupons" element={<Coupons/>} />
-              <Route path="/discounts" element={<Discounts/>} />
-            </Routes>
-          </div>
+      <Navbar />
+      <div className="d-flex">
+        <Sidebar />
+        <div className="pageinfo">
+          <Routes>
+            <Route path='*' element={<ErrorPage />} />
+            <Route path="/home" index element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/analysis" element={<Analysis />} />
+            <Route path="/invoices" element={<Invoice />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/companies" element={<Companies />} />
+            <Route path="/customers" element={<Customers />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/coupons" element={<Coupons />} />
+            <Route path="/discounts" element={<Discounts />} />
+          </Routes>
         </div>
-      </BrowserRouter>
+      </div>
+    </BrowserRouter>
   </StrictMode>
 );
