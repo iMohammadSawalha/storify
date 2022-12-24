91a8103ae0429af6a812ea852b87cfa8b7070029
mohammad-e-sawalha <s12112755@stu.najah.edu>
2022-12-24 21:51:15 +0200

    Modified LDashCards.js to use React-Bootstrap

--- src/Components/Analysis/LDashCards.js
+++ src/Components/Analysis/LDashCards.js
@@ -1,20 +1,22 @@
+import { Card } from "react-bootstrap";
+
 const LargeCard = ({ title, subtitle, data, datatitle, chart }) => {
   return (
-    <div className="card h-100 border-0">
-      <div className="card-header d-flex align-items-center justify-content-between pb-0 border-bottom-0 bg-white">
-        <div className="card-title mb-0">
+    <Card className="border-0">
+      <Card.Header className="d-flex align-items-center justify-content-between pb-0 border-bottom-0 bg-white">
+        <Card.Title className="mb-0">
           <h5 className="m-0 me-2">{title}</h5>
           <small className="text-muted">{subtitle}</small>
-        </div>
-      </div>
-      <div className="card-body d-flex flex-column align-items-center">
+        </Card.Title>
+      </Card.Header>
+      <Card.Body className="d-flex flex-column align-items-center">
         <div>{chart}</div>
         <div className="d-flex align-items-center gap-1">
           <h2 className="mb-2">{data}</h2>
           <span>{datatitle}</span>
         </div>
-      </div>
-    </div>
+      </Card.Body>
+    </Card>
   );
 };
 export default LargeCard;
