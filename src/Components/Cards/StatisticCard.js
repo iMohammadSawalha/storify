e030df8366dda9e677632691c731ce6b2482f25c
Abdulrahman <s12114751@stu.najah.edu>
2022-12-24 20:05:12 +0200

    Colors to large cards

--- src/Components/Cards/StatisticCard.js
+++ src/Components/Cards/StatisticCard.js
@@ -2,6 +2,7 @@ import Card from 'react-bootstrap/Card';
 import React from "react";
 import './card.css';
 import OrdersIcon from "./Icons/OrdersIcons";
+import ProductsIcon from './Icons/ProductsIcons';
 
 export default function StatisticCard({info,data}) {
   return (
@@ -14,12 +15,17 @@ export default function StatisticCard({info,data}) {
         </Card.Header>
         <Card.Body className='d-flex flex-column'>
         {data.map((data)=>
-            <div key={data.id} className='d-flex mb-3'>
+            <div key={data.id} className='d-flex mt-3'>
                 <div className='d-flex' key={data.id+'a'}>
-                    <div className='ordersCardIcon'><OrdersIcon className='my-auto' key={data.id} options={{ id: data.id }}/></div>
-                    <span key={data.id+'b'} className='ms-2'>{data.name}</span>
+                    <div style={{backgroundColor:(data.background)}} className='d-flex ordersCardIcon'>
+                        {info.orderIcon ? 
+                        (<OrdersIcon key={data.id} options={{ color:data.border,id: data.id }}/>) :
+                        (<ProductsIcon  key={data.id} options={{color:data.border, id: data.id }}/>)
+                        }
+                    </div>
+                    <span key={data.id+'b'} className='my-auto ms-2'>{data.name}</span>
                 </div>
-                <span className='text-muted ms-auto'>{data.sales}</span>
+                <span className='text-muted my-auto ms-auto'>{data.sales}</span>
             </div>
         )}
 
