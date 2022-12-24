46b62cebd637dc2fc1ac6c504fa5f1f17af74c3e
mohammad-e-sawalha <s12112755@stu.najah.edu>
2022-12-24 18:06:52 +0200

    From side Tabs to Top Tabs in Analysis Page

--- src/Components/Analysis/TabsCards.js
+++ src/Components/Analysis/TabsCards.js
@@ -2,62 +2,41 @@ import LargeCard from "./LDashCards";
 import PolarChart from "./PolarChart";
 import PieChart from "./PieChart";
 import DoughnutChart from "./DoughnutChart";
-import { Card } from "react-bootstrap";
-import Col from "react-bootstrap/Col";
-import Nav from "react-bootstrap/Nav";
-import Row from "react-bootstrap/Row";
 import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import { Card } from "react-bootstrap";
 function TabsCards() {
   return (
     <Card className="mb-4">
-      <Tab.Container id="left-tabs-example" defaultActiveKey="Traffic">
-        <Row>
-          <Col sm={3}>
-            <Nav variant="pills" className="flex-column">
-              <Nav.Item>
-                <Nav.Link eventKey="Orders">Orders</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="Traffic">Traffic</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link eventKey="Sales">Sales</Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Col>
-          <Col sm={9}>
-            <Tab.Content>
-              <Tab.Pane eventKey="Orders">
-                <LargeCard
-                  title={"Order Statistics"}
-                  subtitle={"42.82k Total Orders"}
-                  data={"8,258"}
-                  datatitle={"Total Orders"}
-                  chart={<DoughnutChart />}
-                />
-              </Tab.Pane>
-              <Tab.Pane eventKey="Traffic">
-                <LargeCard
-                  title={"Traffic Source"}
-                  subtitle={"3.2k Visits last month"}
-                  data={"143"}
-                  datatitle={"Today"}
-                  chart={<PolarChart />}
-                />
-              </Tab.Pane>
-              <Tab.Pane eventKey="Sales">
-                <LargeCard
-                  title={"Most Sold"}
-                  subtitle={"Electronic 82.5k"}
-                  data={"155.4k"}
-                  datatitle={"Total Sold"}
-                  chart={<PieChart />}
-                />
-              </Tab.Pane>
-            </Tab.Content>
-          </Col>
-        </Row>
-      </Tab.Container>
+      <Tabs defaultActiveKey="Traffic" className="mb-3 border-0">
+        <Tab eventKey="Orders" title="Orders">
+          <LargeCard
+            title={"Order Statistics"}
+            subtitle={"42.82k Total Sales"}
+            data={"8,258"}
+            datatitle={"Total Orders"}
+            chart={<DoughnutChart />}
+          />
+        </Tab>
+        <Tab eventKey="Traffic" title="Traffic">
+          <LargeCard
+            title={"Traffic Source"}
+            subtitle={"3.2k Visits last month"}
+            data={"143"}
+            datatitle={"Today"}
+            chart={<PolarChart />}
+          />
+        </Tab>
+        <Tab eventKey="Sales" title="Sales">
+          <LargeCard
+            title={"Most Sold"}
+            subtitle={"Electronic 82.5k"}
+            data={"155.4k"}
+            datatitle={"Total Sold"}
+            chart={<PieChart />}
+          />
+        </Tab>
+      </Tabs>
     </Card>
   );
 }
