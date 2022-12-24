70bd1efd5273956040aa9e6f0fe8295ad510d649
Ayman <s12144191@stu.najah.edu>
2022-12-24 19:28:58 +0200

    Few edits, header and logo added

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
-import Sidebar from "./Components/sidebar/Sidebar";
+import Sidebar from "./Components/Sidebar/Sidebar";
 import "./index.css";
 import Analysis from "./Pages/Analysis/Analysis";
 import Companies from "./Pages/Companies/Companies";
